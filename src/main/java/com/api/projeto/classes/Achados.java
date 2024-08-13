4c19bbcf7538ffa4687552b6d92f045661e30535
PedroVieiraS <pedrocorrea978@gmail.com>
2024-08-13 19:48:28 -0300

    teste

--- src/main/java/com/api/projeto/classes/Achados.java
+++ src/main/java/com/api/projeto/classes/Achados.java
@@ -1,5 +1,28 @@
 package com.api.projeto.classes;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Table(name = "achados")
+@Entity
 public class Achados {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String quemAchou;
+
+    @Column(nullable = false)
+    private String nomeDoItem;
+
+    @Column(nullable = false)
+    private String Categoria;
+
+    @Column(nullable = false)
+    private String DataDeEncontro;
 
 }
