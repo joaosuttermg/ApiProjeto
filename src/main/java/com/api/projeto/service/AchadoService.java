8f8655fcb19f994677d9f00c5cff8fa3e53bb66e
Patrick Tavares dos Santos <ptsantosrj@gmail.com>
2024-08-16 16:43:06 -0300

    Desfazendo a implementacao de tratamento de erros

--- src/main/java/com/api/projeto/service/AchadoService.java
+++ src/main/java/com/api/projeto/service/AchadoService.java
@@ -3,11 +3,11 @@ package com.api.projeto.service;
 import com.api.projeto.classes.Achado;
 import com.api.projeto.dto.AchadoDTO;
 import com.api.projeto.dto.AchadoUpdateDTO;
-import com.api.projeto.exceptions.ErroInesperado;
 import com.api.projeto.repository.AchadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,125 +18,119 @@ public class AchadoService {
     private AchadoRepository achadoRepository;
 
     public List<Achado> getAll() {
-        try {
-            return achadoRepository.findAll();
-        } catch (Exception e) {
-            throw new ErroInesperado("Erro ao buscar todos os achados: " + e.getMessage());
-        }
+        return achadoRepository.findAll();
     }
 
     public Achado getById(Long id) {
-        try {
-            return achadoRepository.findById(id)
-                                    .orElseThrow(() -> new ErroInesperado("Achado não encontrado com o ID: " + id));
-        } catch (Exception e) {
-            throw new ErroInesperado("Erro ao buscar achado por ID: " + e.getMessage());
-        }
+        return achadoRepository.findById(id)
+                                .orElse(null);
     }
+    // public List<Achado> getCategoria(String categoria) {
+    //     // Lógica para buscar itens por categoria
+    //     Achado achado = achadoService.getByCategoria(categoria);
+    //     return achadoRepository.findByCategoria(categoria);
+                                
+                                
+    // }
 
     public Achado create(Achado achado) {
-        try {
-            return achadoRepository.save(achado);
-        } catch (Exception e) {
-            throw new ErroInesperado("Erro ao criar um novo achado: " + e.getMessage());
-        }
+        // Adicionar tratamentos para garantir que a persistencia 
+        // acontece com todos os dados necessários
+        Achado achadoSalvo = achadoRepository.save(achado);
+        return achadoSalvo;
     }
 
     public Achado update(Long id, Achado achadoExistente, Achado achadoNovo) {
-        try {
-            if (achadoNovo.getQuemAchou() != null) {
-                achadoExistente.setQuemAchou(achadoNovo.getQuemAchou());
-            }
-            if (achadoNovo.getNomeDoItem() != null) {
-                achadoExistente.setNomeDoItem(achadoNovo.getNomeDoItem());
-            }
-            if (achadoNovo.getCategoria() != null) {
-                achadoExistente.setCategoria(achadoNovo.getCategoria());
-            }
-            if (achadoNovo.getDataDeEncontro() != null) {
-                achadoExistente.setDataDeEncontro(achadoNovo.getDataDeEncontro());
-            }
-            if (achadoNovo.getLocalDeEncontro() != null) {
-                achadoExistente.setLocalDeEncontro(achadoNovo.getLocalDeEncontro());
-            }
-            if (achadoNovo.isAchadoAtivo() != achadoExistente.isAchadoAtivo()) {
-                achadoExistente.setAchadoAtivo(achadoNovo.isAchadoAtivo());
-            }
 
-            return achadoRepository.save(achadoExistente);
-        } catch (Exception e) {
-            throw new ErroInesperado("Erro ao atualizar o achado: " + e.getMessage());
+        if (achadoNovo.getQuemAchou() != null) {
+            achadoExistente.setQuemAchou(achadoNovo.getQuemAchou());
         }
+        if (achadoNovo.getNomeDoItem() != null) {
+            achadoExistente.setNomeDoItem(achadoNovo.getNomeDoItem());
+        }
+        if (achadoNovo.getCategoria() != null) {
+            achadoExistente.setCategoria(achadoNovo.getCategoria());
+        }
+        if (achadoNovo.getDataDeEncontro() != null) {
+            achadoExistente.setDataDeEncontro(achadoNovo.getDataDeEncontro());
+        }
+        if (achadoNovo.getLocalDeEncontro() != null) {
+            achadoExistente.setLocalDeEncontro(achadoNovo.getLocalDeEncontro());
+        }
+        if (achadoNovo.isAchadoAtivo() != achadoExistente.isAchadoAtivo()) {
+            achadoExistente.setAchadoAtivo(achadoNovo.isAchadoAtivo());
+        }
+
+        return achadoRepository.save(achadoExistente);
     }
 
     public Achado delete(Long id) {
-        try {
-            Achado achado = getById(id);
-            achadoRepository.deleteById(id);  // Remoção física
-            achado.setAchadoAtivo(false);    // Remoção lógica
-            return achadoRepository.save(achado);
-        } catch (Exception e) {
-            throw new ErroInesperado("Erro ao deletar o achado: " + e.getMessage());
-        }
+        // Delete anterior
+        achadoRepository.deleteById(id);
+
+        // Delete lógico
+        Achado achado = getById(id);
+        achado.setAchadoAtivo(false);
+
+        return achadoRepository.save(achado);
     }
 
     public List<Achado> getAllAtivos() {
-        try {
-            return achadoRepository.findByAchadoAtivoTrue();
-        } catch (Exception e) {
-            throw new ErroInesperado("Erro ao buscar todos os achados ativos: " + e.getMessage());
-        }
+        return achadoRepository.findByAchadoAtivoTrue();
     }
 
+
+
     public AchadoUpdateDTO updateDTO(Achado achadoExistente, AchadoUpdateDTO achadoNovo) {
-        try {
-            if (achadoNovo.getQuemAchou() != null) {
-                achadoExistente.setQuemAchou(achadoNovo.getQuemAchou());
-            }
-            if (achadoNovo.getNomeDoItem() != null) {
-                achadoExistente.setNomeDoItem(achadoNovo.getNomeDoItem());
-            }
-            if (achadoNovo.getCategoria() != null) {
-                achadoExistente.setCategoria(achadoNovo.getCategoria());
-            }
-            if (achadoNovo.getDataDeEncontro() != null) {
-                achadoExistente.setDataDeEncontro(achadoNovo.getDataDeEncontro());
-            }
-            if (achadoNovo.getLocalDeEncontro() != null) {
-                achadoExistente.setLocalDeEncontro(achadoNovo.getLocalDeEncontro());
-            }
 
-            Achado achadoSalvo = achadoRepository.save(achadoExistente);
+        // Converter o que é DTO pra Achado
 
-            AchadoUpdateDTO achadoDTO = new AchadoUpdateDTO();
-            achadoDTO.setId(achadoSalvo.getId());
-            achadoDTO.setQuemAchou(achadoSalvo.getQuemAchou());
-            achadoDTO.setNomeDoItem(achadoSalvo.getNomeDoItem());
-            achadoDTO.setCategoria(achadoSalvo.getCategoria());
-            achadoDTO.setDataDeEncontro(achadoSalvo.getDataDeEncontro());
-            achadoDTO.setLocalDeEncontro(achadoSalvo.getLocalDeEncontro());
-
-            return achadoDTO;
-        } catch (Exception e) {
-            throw new ErroInesperado("Erro ao atualizar o achado DTO: " + e.getMessage());
+        if (achadoNovo.getQuemAchou() != null) {
+            achadoExistente.setQuemAchou(achadoNovo.getQuemAchou());
         }
+        if (achadoNovo.getNomeDoItem() != null) {
+            achadoExistente.setNomeDoItem(achadoNovo.getNomeDoItem());
+        }
+        if (achadoNovo.getCategoria() != null) {
+            achadoExistente.setCategoria(achadoNovo.getCategoria());
+        }
+        if (achadoNovo.getDataDeEncontro() != null) {
+            achadoExistente.setDataDeEncontro(achadoNovo.getDataDeEncontro());
+        }
+        if (achadoNovo.getLocalDeEncontro() != null) {
+            achadoExistente.setLocalDeEncontro(achadoNovo.getLocalDeEncontro());
+        }
+
+        // Atualizar o achadoExistente com os dados do achadoNovo
+        Achado achadoSalvo = achadoRepository.save(achadoExistente);
+
+        // Converter o Achado pra DTO 
+        AchadoUpdateDTO achadoDTO = new AchadoUpdateDTO();
+        achadoDTO.setId(achadoSalvo.getId());
+        achadoDTO.setQuemAchou(achadoSalvo.getQuemAchou());
+        achadoDTO.setNomeDoItem(achadoSalvo.getNomeDoItem());
+        achadoDTO.setCategoria(achadoSalvo.getCategoria());
+        achadoDTO.setDataDeEncontro(achadoSalvo.getDataDeEncontro());
+        achadoDTO.setLocalDeEncontro(achadoSalvo.getLocalDeEncontro());
+
+        // Retornar
+        return achadoDTO;
     }
 
     public List<AchadoDTO> getAchadosDTO() {
-        try {
-            List<Achado> achados = achadoRepository.findAll();
-            List<AchadoDTO> achadosDTO = new ArrayList<>();
 
-            for (Achado achado : achados) {
-                AchadoDTO achadoDTO = new AchadoDTO();
-                achadoDTO.setId(achado.getId());
-                achadoDTO.setNomeDoItem(achado.getNomeDoItem());
+        List<Achado> achados = achadoRepository.findAll();
 
-                achadosDTO.add(achadoDTO);
-            }
-            return achadosDTO;
-        } catch (Exception e) {
-            throw new ErroInesperado("Erro ao buscar os achados DTO: " + e.getMessage());
+        List<AchadoDTO> achadosDTO = new ArrayList<>();
+
+        for (Achado achado : achados) {
+            AchadoDTO achadoDTO = new AchadoDTO();
+            achadoDTO.setId(achado.getId());
+            achadoDTO.setNomeDoItem(achado.getNomeDoItem());
+
+            achadosDTO.add(achadoDTO);
         }
+        return achadosDTO;
     }
+   
 }
--- src/main/java/com/api/projeto/service/PerdidoService.java
+++ src/main/java/com/api/projeto/service/PerdidoService.java
@@ -1,15 +1,18 @@
 package com.api.projeto.service;
 
+import com.api.projeto.classes.Achado;
 import com.api.projeto.classes.Perdido;
+import com.api.projeto.dto.AchadoDTO;
+import com.api.projeto.dto.AchadoUpdateDTO;
 import com.api.projeto.dto.PerdidoDTO;
 import com.api.projeto.dto.PerdidoUpdateDTO;
-import com.api.projeto.exceptions.ErroInesperado;
 import com.api.projeto.repository.PerdidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Optional;
 
 @Service
@@ -19,118 +22,100 @@ public class PerdidoService {
     private PerdidoRepository perdidoRepository;
 
     public List<Perdido> getAll() {
-        try {
-            return perdidoRepository.findAll();
-        } catch (Exception e) {
-            throw new ErroInesperado("Erro ao buscar todos os perdidos.");
-        }
+        return perdidoRepository.findAll();
     }
 
     public Perdido getById(Long id) {
-        try {
-            return perdidoRepository.findById(id).orElseThrow(() -> new ErroInesperado("Perdido não encontrado com o ID: " + id));
-        } catch (Exception e) {
-            throw new ErroInesperado("Erro ao buscar o perdido com o ID: " + id);
-        }
+        return perdidoRepository.findById(id).orElse(null);
     }
 
     public Perdido create(Perdido perdido) {
-        try {
-            return perdidoRepository.save(perdido);
-        } catch (Exception e) {
-            throw new ErroInesperado("Erro ao criar um novo perdido.");
+        return perdidoRepository.save(perdido);
+    }
+    public List<PerdidoDTO> getPerdidoDTO() {
+
+        List<Perdido> perdidos = perdidoRepository.findAll();
+
+        List<PerdidoDTO> perdidosDTO = new ArrayList<>();
+
+        for (Perdido perdido : perdidos) {
+            PerdidoDTO perdidoDTO = new PerdidoDTO();
+            perdidoDTO.setId(perdido.getId());
+            perdidoDTO.setNomeDoItem(perdido.getNomeDoItem());
+
+            perdidosDTO.add(perdidoDTO);
         }
+        return perdidosDTO;
     }
 
     public Perdido update(Long id, Perdido perdidoExistente, Perdido perdidoNovo) {
-        try {
-            if (perdidoNovo.getQuemPerdeu() != null) {
-                perdidoExistente.setQuemPerdeu(perdidoNovo.getQuemPerdeu());
-            }
-            if (perdidoNovo.getNomeDoItem() != null) {
-                perdidoExistente.setNomeDoItem(perdidoNovo.getNomeDoItem());
-            }
-            if (perdidoNovo.getDataDaPerda() != null) {
-                perdidoExistente.setDataDaPerda(perdidoNovo.getDataDaPerda());
-            }
-            if (perdidoNovo.getLocalDaPerda() != null) {
-                perdidoExistente.setLocalDaPerda(perdidoNovo.getLocalDaPerda());
-            }
-            if (perdidoNovo.getDescricaoDoItem() != null) {
-                perdidoExistente.setDescricaoDoItem(perdidoNovo.getDescricaoDoItem());
-            }
-            return perdidoRepository.save(perdidoExistente);
-        } catch (Exception e) {
-            throw new ErroInesperado("Erro ao atualizar o perdido com o ID: " + id);
-        }
-    }
 
+        if (perdidoNovo.getQuemPerdeu() != null) {
+            perdidoExistente.setQuemPerdeu(perdidoNovo.getQuemPerdeu());
+        }
+        if (perdidoNovo.getNomeDoItem() != null){
+            perdidoExistente.setNomeDoItem(perdidoNovo.getNomeDoItem());
+        }
+        if (perdidoNovo.getDataDaPerda() != null){
+            perdidoExistente.setDataDaPerda(perdidoNovo.getDataDaPerda());
+        }
+        if (perdidoNovo.getLocalDaPerda()!= null){
+            perdidoExistente.setLocalDaPerda(perdidoNovo.getLocalDaPerda());
+        }
+        if (perdidoNovo.getDescricaoDoItem()!= null){
+            perdidoExistente.setDescricaoDoItem(perdidoNovo.getDescricaoDoItem());
+        }
+        return perdidoRepository.save(perdidoExistente);
+    }
     public PerdidoUpdateDTO updateDTO(Perdido perdidoExistente, PerdidoUpdateDTO perdidoNovo) {
-        try {
-            if (perdidoNovo.getQuemPerdeu() != null) {
-                perdidoExistente.setQuemPerdeu(perdidoNovo.getQuemPerdeu());
-            }
-            if (perdidoNovo.getNomeDoItem() != null) {
-                perdidoExistente.setNomeDoItem(perdidoNovo.getNomeDoItem());
-            }
-            if (perdidoNovo.getCategoria() != null) {
-                perdidoExistente.setCategoria(perdidoNovo.getCategoria());
-            }
-            if (perdidoNovo.getDataDaPerda() != null) {
-                perdidoExistente.setDataDaPerda(perdidoNovo.getDataDaPerda());
-            }
-            if (perdidoNovo.getLocalDaPerda() != null) {
-                perdidoExistente.setLocalDaPerda(perdidoNovo.getLocalDaPerda());
-            }
 
-            Perdido perdidoSalvo = perdidoRepository.save(perdidoExistente);
+        // Converter o que é DTO pra Perdido
 
-            PerdidoUpdateDTO perdidoDTO = new PerdidoUpdateDTO();
-            perdidoDTO.setId(perdidoSalvo.getId());
-            perdidoDTO.setQuemPerdeu(perdidoSalvo.getQuemPerdeu());
-            perdidoDTO.setNomeDoItem(perdidoSalvo.getNomeDoItem());
-            perdidoDTO.setCategoria(perdidoSalvo.getCategoria());
-            perdidoDTO.setDataDaPerda(perdidoSalvo.getDataDaPerda());
-            perdidoDTO.setLocalDaPerda(perdidoSalvo.getLocalDaPerda());
-
-            return perdidoDTO;
-        } catch (Exception e) {
-            throw new ErroInesperado("Erro ao atualizar o DTO do perdido.");
+        if (perdidoNovo.getQuemPerdeu() != null) {
+            perdidoExistente.setQuemPerdeu(perdidoNovo.getQuemPerdeu());
         }
+        if (perdidoNovo.getNomeDoItem() != null) {
+            perdidoExistente.setNomeDoItem(perdidoNovo.getNomeDoItem());
+        }
+        if (perdidoNovo.getCategoria() != null) {
+            perdidoExistente.setCategoria(perdidoNovo.getCategoria());
+        }
+        if (perdidoNovo.getDataDaPerda() != null) {
+            perdidoExistente.setDataDaPerda(perdidoNovo.getDataDaPerda());
+        }
+        if (perdidoNovo.getLocalDaPerda() != null) {
+            perdidoExistente.setLocalDaPerda(perdidoNovo.getLocalDaPerda());
+        }
+
+        // Atualizar o achadoExistente com os dados do achadoNovo
+        Perdido perdidoSalvo = perdidoRepository.save(perdidoExistente);
+
+        // Converter o Achado pra DTO 
+        PerdidoUpdateDTO perdidoDTO = new PerdidoUpdateDTO();
+        perdidoDTO.setId(perdidoSalvo.getId());
+        perdidoDTO.setQuemPerdeu(perdidoSalvo.getQuemPerdeu());
+        perdidoDTO.setNomeDoItem(perdidoSalvo.getNomeDoItem());
+        perdidoDTO.setCategoria(perdidoSalvo.getCategoria());
+        perdidoDTO.setDataDaPerda(perdidoSalvo.getDataDaPerda());
+        perdidoDTO.setLocalDaPerda(perdidoSalvo.getLocalDaPerda());
+
+        // Retornar
+        return perdidoDTO;
     }
+
 
     public Perdido delete(Long id) {
-        try {
-            Perdido perdido = getById(id);
-            perdido.setPerdidoAtivo(false);
-            return perdidoRepository.save(perdido);
-        } catch (Exception e) {
-            throw new ErroInesperado("Erro ao deletar o perdido com o ID: " + id);
-        }
-    }
+        // Delete anterior
+        perdidoRepository.deleteById(id);
 
-    public List<Perdido> getAllAtivos() {
-        try {
-            return perdidoRepository.findByPerdidoAtivoTrue();
-        } catch (Exception e) {
-            throw new ErroInesperado("Erro ao buscar todos os perdidos ativos.");
-        }
-    }
+        // Delete lógico
+        Perdido perdido = getById(id);
+        perdido.setPerdidoAtivo(false);
 
-    public List<PerdidoDTO> getPerdidoDTO() {
-        try {
-            List<Perdido> perdidos = perdidoRepository.findAll();
-            List<PerdidoDTO> perdidosDTO = new ArrayList<>();
-
-            for (Perdido perdido : perdidos) {
-                PerdidoDTO perdidoDTO = new PerdidoDTO();
-                perdidoDTO.setId(perdido.getId());
-                perdidoDTO.setNomeDoItem(perdido.getNomeDoItem());
-                perdidosDTO.add(perdidoDTO);
-            }
-            return perdidosDTO;
-        } catch (Exception e) {
-            throw new ErroInesperado("Erro ao buscar os perdidos como DTO.");
-        }
+        return perdidoRepository.save(perdido);
     }
+       public List<Perdido> getAllAtivos() {
+        return perdidoRepository.findByPerdidoAtivoTrue();
+    }
+   
 }
